3ab0c93ad970c07228d271538a7a0f0fa872cfec
victor-egilla <40365536+victor-egilla@users.noreply.github.com>
2021-09-23 12:41:50 +0100

    Added new source code

---
+++ Multiple_File_Upload_Program/Models/Documents.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Multiple_File_Upload_Program.Models
+{
+    public class Documents
+    {
+    }
+}
---
+++ Multiple_File_Upload_Program/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Multiple_File_Upload_Program.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
